d7a7a99a52783ef712d0d4a7f7b8a8213b0e49f4
Luiza.Patroescu <luiza.patroescu@trencadis.ro>
2021-09-16 01:05:01 -0700

    photo working

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Photo, PhotoService } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,45 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public photoBuletin: PhotoService,
+    public photoSelfie: PhotoService,
+    public actionSheetController: ActionSheetController) {}
 
+  async ngOnInit() {
+    await this.photoBuletin.loadSaved(0);
+    await this.photoSelfie.loadSaved(1);
+  }
+  addPhotoBToGallery() {
+    this.photoBuletin.index = 0;
+    this.photoBuletin.addNewToGallery();
+
+  }
+  addPhotoSToGallery() {
+    this.photoSelfie.index = 1;
+    this.photoSelfie.addNewToGallery();
+
+  }
+
+  public async showActionSheet(photo: Photo, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoBuletin.deletePicture(photo, position);
+          this.photoSelfie.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
 }
